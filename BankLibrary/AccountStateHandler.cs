bbf7778fb3eace618c634352c298be2784ddb79e
dmytro_h <nedvancrest@gmail.com>
2016-08-18 18:57:29 +0200

    #1
    #1
    1. IAccount
    2. AccountStateHandler

---
+++ BankLibrary/AccountStateHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public delegate void AccountStateHandler(object sender, AccountEventsArgs e);
+
+    public class AccountEventsArgs
+    {
+        // Message
+        public string Message { get; private set; }
+        // Amount of money that has been changed
+        public decimal Sum { get; private set; }
+
+        public AccountEventsArgs(string _mes, decimal _sum)
+        {
+            Message = _mes;
+            Sum = _sum;
+        }
+    }
+
+}
